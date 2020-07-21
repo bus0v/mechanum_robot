d5d1704bb804cd6fc5ac0c844d272e9971a9c957
Petr Busov <petr.busov@dal.ca>
2020-07-21 19:02:07 +0100

    add robot.py and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+#robot
+This project is dedicated to my raspberry pi robot. It has two motors and is differentially driven.
---
+++ robot.py
@@ -0,0 +1,94 @@
+import time
+import sys
+import tkinter as tk
+import RPi.GPIO as GPIO
+mode=GPIO.getmode()
+print(str(mode))
+en=5
+in3=16
+in4=20
+temp1=1
+enb=21
+in1=6
+in2=13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.setup(enb,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+p.start(50)
+o=GPIO.PWM(enb,1000)
+o.start(50)
+print("/n")
+print("default speed and dir is low and forward")
+print("use WASD keys to control the robot")
+run=True
+delay=0.001
+
+while(run):
+	 try:
+	  k=key.char
+	 except:
+	  print("please use a wasdq characters")
+	 if k=="w":
+	  GPIO.output(in1,GPIO.LOW)
+	  GPIO.output(in2,GPIO.HIGH)
+	  GPIO.output(in3,GPIO.HIGH)
+	  GPIO.output(in4,GPIO.LOW)
+	  time.sleep(delay)
+	  GPIO.output(in2,GPIO.LOW)
+	  GPIO.output(in3,GPIO.LOW)
+	  GPIO.output(in4,GPIO.LOW)
+	  GPIO.output(in1,GPIO.LOW)
+
+	  print("forward")
+	 if k=="s":
+          GPIO.output(in1,GPIO.HIGH)
+          GPIO.output(in2,GPIO.LOW)
+          GPIO.output(in3,GPIO.LOW)
+          GPIO.output(in4,GPIO.HIGH)
+          time.sleep(delay)
+          GPIO.output(in2,GPIO.LOW)
+          GPIO.output(in3,GPIO.LOW)
+          GPIO.output(in4,GPIO.LOW)
+          GPIO.output(in1,GPIO.LOW)
+
+          print("retreat")
+	 if k=="d":
+          GPIO.output(in1,GPIO.HIGH)
+          GPIO.output(in2,GPIO.LOW)
+          GPIO.output(in3,GPIO.HIGH)
+          GPIO.output(in4,GPIO.LOW)
+          time.sleep(delay)
+          GPIO.output(in2,GPIO.LOW)
+          GPIO.output(in3,GPIO.LOW)
+          GPIO.output(in4,GPIO.LOW)
+          GPIO.output(in1,GPIO.LOW)
+          print("right")
+	 if k=="a":
+	  GPIO.output(in1,GPIO.LOW)
+	  GPIO.output(in2,GPIO.HIGH)
+	  GPIO.output(in3,GPIO.LOW)
+	  GPIO.output(in4,GPIO.HIGH)
+	  time.sleep(delay)
+	  GPIO.output(in2,GPIO.LOW)
+	  GPIO.output(in3,GPIO.LOW)
+	  GPIO.output(in4,GPIO.LOW)
+	  GPIO.output(in1,GPIO.LOW)
+	  print("left")
+
+	 if k=="q":
+	  print("stop")
+	  run=False
+	 #GPIO.output(in1,GPIO.LOW)
+	 #GPIO.output(in2,GPIO.LOW)
+	 #GPIO.output(in3,GPIO.LOW)
+	 #GPIO.output(in4,GPIO.LOW)	
+GPIO.cleanup()
