f45dfe7dd411394542c60b0cb9897a733559da1c
bus0v <51008991+bus0v@users.noreply.github.com>
2021-01-26 14:40:17 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -6,27 +6,37 @@ import tkinter as tk
 import RPi.GPIO as GPIO
 mode=GPIO.getmode()
 print(str(mode))
-en=5
-in3=16
-in4=20
-temp1=1
-enb=21
-in1=6
-in2=13
+#setup motor pins
+#left
+Lenb=2
+Lin4=3
+Lin3=4
+Lin2=17
+Lin1=27
+Lena=22
+
+Rena=11
+Rin1=5
+Rin2=6
+Rin3=13
+Rin4=19
+Renb=26
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.setup(enb,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+GPIO.setup(Lin1,GPIO.OUT)
+GPIO.setup(Lin2,GPIO.OUT)
+GPIO.setup(Lin3,GPIO.OUT)
+GPIO.setup(Lin4,GPIO.OUT)
+GPIO.setup(Lena,GPIO.OUT)
+GPIO.setup(Lenb,GPIO.OUT)
+GPIO.output(Lin1,GPIO.LOW)
+GPIO.output(Lin2,GPIO.LOW)
+GPIO.output(Lin3,GPIO.LOW)
+GPIO.output(Lin4,GPIO.LOW)
+
+p=GPIO.PWM(Lena,1000)
 p.start(50)
-o=GPIO.PWM(enb,1000)
+o=GPIO.PWM(Lenb,1000)
 o.start(50)
 print("/n")
 print("default speed and dir is low and forward")
@@ -34,70 +44,58 @@ print("default speed and dir is low and forward")
 run=True
 delay=0.001
 
-def forward(delay):
+# def forward(delay):
 
-	GPIO.output(in1,GPIO.LOW)
-	GPIO.output(in2,GPIO.HIGH)
-	GPIO.output(in3,GPIO.HIGH)
-	GPIO.output(in4,GPIO.LOW)
-	time.sleep(delay)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.LOW)
-	GPIO.output(in1,GPIO.LOW)
-	print("forward")
+# 	GPIO.output(in1,GPIO.LOW)
+# 	GPIO.output(in2,GPIO.HIGH)
+# 	GPIO.output(in3,GPIO.HIGH)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	time.sleep(delay)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	GPIO.output(in1,GPIO.LOW)
+# 	print("forward")
 
-def backward(delay):
-	GPIO.output(in1,GPIO.HIGH)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.HIGH)
-	time.sleep(delay)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.LOW)
-	GPIO.output(in1,GPIO.LOW)
-	print("retreat")
+# def backward(delay):
+# 	GPIO.output(in1,GPIO.HIGH)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.HIGH)
+# 	time.sleep(delay)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	GPIO.output(in1,GPIO.LOW)
+# 	print("retreat")
 	
-def right(delay):
-	GPIO.output(in1,GPIO.HIGH)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.HIGH)
-	GPIO.output(in4,GPIO.LOW)
-	time.sleep(delay)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.LOW)
-	GPIO.output(in1,GPIO.LOW)
-	print("right")
+# def right(delay):
+# 	GPIO.output(in1,GPIO.HIGH)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.HIGH)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	time.sleep(delay)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	GPIO.output(in1,GPIO.LOW)
+# 	print("right")
 	 
-def left(delay):
+# def left(delay):
 
-	GPIO.output(in1,GPIO.LOW)
-	GPIO.output(in2,GPIO.HIGH)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.HIGH)
-	time.sleep(delay)
-	GPIO.output(in2,GPIO.LOW)
-	GPIO.output(in3,GPIO.LOW)
-	GPIO.output(in4,GPIO.LOW)
-	GPIO.output(in1,GPIO.LOW)
-	print("left")
+# 	GPIO.output(in1,GPIO.LOW)
+# 	GPIO.output(in2,GPIO.HIGH)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.HIGH)
+# 	time.sleep(delay)
+# 	GPIO.output(in2,GPIO.LOW)
+# 	GPIO.output(in3,GPIO.LOW)
+# 	GPIO.output(in4,GPIO.LOW)
+# 	GPIO.output(in1,GPIO.LOW)
+# 	print("left")
 	
-while(run):
-	 try:
-	  k=key.char
-	 except:
-	  print("please use a wasdq characters")
-	 if k=="w":
-	 if k=="s":
-	 if k=="q":
-	 if k=="d":
-	 if k=="a":
-	  print("stop")
-	  run=False
-	 #GPIO.output(in1,GPIO.LOW)
-	 #GPIO.output(in2,GPIO.LOW)
-	 #GPIO.output(in3,GPIO.LOW)
-	 #GPIO.output(in4,GPIO.LOW)	
-GPIO.cleanup()
+print("testing FL motor")
+print("forward")
+GPIO.output(Lin1,GPIO.HIGH)
+GPIO.output(Lin2,GPIO.LOW)
+print("forward")
