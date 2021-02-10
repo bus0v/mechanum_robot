2a17fcb672d4fccc37f9aadc8e6458e97b900dc5
bus0v <51008991+bus0v@users.noreply.github.com>
2021-02-10 14:54:19 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,110 +1,18 @@
 #Petr Busov
-#This script governs the motion of the robot
+#This is the master script
+#testing serial communication
+
 import time
 import sys
-import tkinter as tk
 import RPi.GPIO as GPIO
+import serial
 
-#setup motor pins
-#left
-Lenb=2
-Lin4=3
-Lin3=4
-Lin2=17
-Lin1=27
-Lena=22
-
-Rena=11
-Rin1=5
-Rin2=6
-Rin3=13
-Rin4=19
-Renb=26
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(Lin1,GPIO.OUT)
-GPIO.setup(Lin2,GPIO.OUT)
-GPIO.setup(Lin3,GPIO.OUT)
-GPIO.setup(Lin4,GPIO.OUT)
-#GPIO.setup(Lena,GPIO.OUT)
-#GPIO.setup(Lenb,GPIO.OUT)
-GPIO.setup(Rin1,GPIO.OUT)
-GPIO.setup(Rin2,GPIO.OUT)
-GPIO.setup(Rin3,GPIO.OUT)
-GPIO.setup(Rin4,GPIO.OUT)
-#GPIO.setup(Rena,GPIO.OUT)
-#GPIO.setup(Renb,GPIO.OUT)
-
-GPIO.output(Lin1,GPIO.LOW)
-GPIO.output(Lin2,GPIO.LOW)
-GPIO.output(Lin3,GPIO.LOW)
-GPIO.output(Lin4,GPIO.LOW)
-GPIO.output(Rin1,GPIO.LOW)
-GPIO.output(Rin2,GPIO.LOW)
-GPIO.output(Rin3,GPIO.LOW)
-GPIO.output(Rin4,GPIO.LOW)
-
-# p=GPIO.PWM(Lena,1000)
-# p.start(100)
-# o=GPIO.PWM(Lenb,1000)
-# o.start(100)
-# b=GPIO.PWM(Renb,1000)
-# b.start(100)
-# c=GPIO.PWM(Rena,1000)
-# c.start(100)
-
-print("/n")
-print("default speed and dir is low and forward")
-
-run=True
-delay=0.001
-	
-def turnon(pin):
-    GPIO.output(pin,GPIO.HIGH)
-    return
-def turnoff(pin):
-    GPIO.output(pin,GPIO.LOW)
-    return
-def motor(position,direction):
-    #f for forward
-    # b for bcakward
-    
-    if position=="br":
-        #fwd doesnt work
-        first=Lin2
-        second=Lin1
-    elif position == "fr":
-        first=Lin4
-        second=Lin3
-    elif position == "fl":
-        first=Rin2
-        second=Rin1
-    elif position == "bl":
-        first=Rin3
-        second=Rin4
-    else:
-        print("invalid motor")
-
-    if direction=="f":
-        turnon(first)
-        turnoff(second)
-    elif direction=="b":
-        turnon(second)
-        turnoff(first)  
-    elif direction=="s":
-        turnoff(second)
-        turnoff(first) 
-    else:
-        print("badinput")
-    return
-position=""
-direction=""
-while(position!=""):
-
-    position=input("which motor do you want to run on? (fr, fl,br,bl): ")
-    direction=input("forward f backward b or stop s ?")
-    motor(position,direction)
-
-
+if __name__=='__main__':
+    ser = serial.Serial('/dev/ttyACM0',9600,timeout=1)
+    ser.flush()
+while True:
+    if ser.in_waiting > 0:
+        line=ser.readline().decode('utf-8'),rstrip()
+        print(line)
 GPIO.cleanup()
 print("GPIO cleaned up")
