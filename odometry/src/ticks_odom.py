60a03f779d20078f27778f418babdbb51c77e4ef
Petr <petr.busov@dal.ca>
2023-01-07 00:17:26 +0000

    fixed the odometry script to properly report distances in the odom frame

--- odometry/src/ticks_odom.py
+++ odometry/src/ticks_odom.py
@@ -16,8 +16,8 @@ import geometry_msgs.msg
 class OdomTf:
     def __init__(self):
         # initialise node
-        rospy.init_node('ticks_reciever',anonymous = True)
-        self.rate =  rospy.get_param("~rate",10.0)
+        rospy.init_node('odometryTF',anonymous = True)
+        self.rate =  rospy.get_param("~rate",0.5)
         # define variables
         # half width in cm
         self.d1 = 10.70
@@ -61,67 +61,78 @@ class OdomTf:
         self.min_time = rospy.Time.now() + self.t_delta
         self.last_time = rospy.Time.now()
 
-        rospy.Subscriber("ticks",Int16MultiArray,self.ticks_reciever)
-        rospy.Subscriber("v_filtered",Int16MultiArray,self.vel_reciever)
+        self.sub_ticks = rospy.Subscriber("ticks",Int16MultiArray, self.ticks_receiver)
+        self.sub_vel = rospy.Subscriber("v_filtered",Int16MultiArray, self.vel_receiver)
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size = 50)
         self.odom_broadcaster = tf2_ros.TransformBroadcaster()
-
+        rospy.loginfo("Initilization complete")
     def spin(self):
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             self.update()
             rate.sleep()
 
-    def ticks_reciever(self,message):
+    def ticks_receiver(self, message):
         #recieve and record encoder ticks
-        self.fr_ticks = message.data[0]
-        self.fl_ticks = message.data[1]
-        self.bl_ticks = message.data[2]
-        self.br_ticks = message.data[3]
+        rospy.loginfo("I got ticks")
+        rospy.loginfo(message.data[0])
+        if message.data[0]!=0:
+            self.fr_ticks = message.data[0]
+        if message.data[1]!=0:
+            self.fl_ticks = message.data[1]
+        if message.data[2]!=0:
+            self.bl_ticks = message.data[2]
+        if message.data[3]!=0:
+            self.br_ticks = message.data[3]
+        else:
+            rospy.loginfo("No new ticks! ")
 
-    def vel_reciever(self, message):
+
+    def vel_receiver(self, message):
+        rospy.loginfo("I got speed")
         self.fr_vel = message.data[0]
         self.fl_vel = message.data[1]
         self.bl_vel = message.data[2]
         self.br_vel = message.data[3]
 
     def update(self):
-
         now = rospy.Time.now()
-
         if now > self.min_time:
             time_elapsed = now - self.last_time
             time_elapsed = time_elapsed.to_sec()
             self.then = now
             #translate encoder ticks into distance each wheel rotated in cm
+            rospy.loginfo("\n fr_ticks: %.2f",self.fr_ticks)
+            rospy.loginfo("\nfr_prev: %d ",self.fr_ticks_prev)
             self.fr_distance = (self.fr_ticks-self.fr_ticks_prev)/330 * 2 * self.R * math.pi
             self.fl_distance = (self.fl_ticks-self.fl_ticks_prev)/330 * 2 * self.R * math.pi
             self.bl_distance = (self.bl_ticks-self.bl_ticks_prev)/330 * 2 * self.R * math.pi
             self.br_distance = (self.br_ticks-self.br_ticks_prev)/330 * 2 * self.R * math.pi
-            rospy.loginfo("\nfr_rotation: %d \nfl_rotation %d",self.fr_distance,self.fl_distance)
-            rospy.loginfo("\nbr_rotation: %d \nbl_rotation %d",self.br_distance,self.bl_distance)
+            rospy.loginfo("\nfr_rotation: %.2f \nfl_rotation: %.2f",self.fr_distance,self.fl_distance)
+            #rospy.loginfo("\nbr_rotation: %d \nbl_rotation: %d",self.br_distance,self.bl_distance)
             self.fr_ticks_prev = self.fr_ticks
             self.fl_ticks_prev = self.fl_ticks
             self.bl_ticks_prev = self.bl_ticks
             self.br_ticks_prev = self.br_ticks
-
             #calculate distances using forward kinematics in cm for the robot in the local frame
-            self.x = (self.fr_distance + self.fl_distance + self.bl_distance + self.br_distance) / 4
-            self.y = (self.fr_distance - self.fl_distance + self.bl_distance - self.br_distance) / 4
-            rospy.loginfo("\nx_distance: %d ",self.x)
-            rospy.loginfo("\ny_distance: %d ",self.y)
+            self.x_traveled = (self.fr_distance + self.fl_distance + self.bl_distance + self.br_distance) / 4
+            self.y_traveled = (self.fr_distance - self.fl_distance + self.bl_distance - self.br_distance) / 4
+            rospy.loginfo("\nx_traveled: %.2f ",self.x_traveled)
+            rospy.loginfo("\ny_traveled: %.2f ",self.y_traveled)
             #calculate theta
-            self.theta = (self.fr_distance - self.fl_distance - self.bl_distance + self.br_distance)  / (4*(self.d1+self.d2))
-            rospy.loginfo("\ntheta: %d ",self.theta)
+            self.theta_traveled = (self.fr_distance - self.fl_distance - self.bl_distance + self.br_distance)  / (4*(self.d1+self.d2))
+            #rospy.loginfo("\ntheta: %d ",self.theta)
             #calculate speed
             self.x_dot = self.x/time_elapsed
             self.y_dot = self.y/time_elapsed
             self.theta_dot = self.theta/time_elapsed
 
             #Calculate distance moved total in the global frame
-            self.x = self.x + (math.cos(self.theta) * self.x) - math.sin(self.theta) * self.y
-            self.y = self.y + (math.sin(self.theta) * self.x) + math.cos(self.theta) * self.y
-            self.theta = self.theta + self.theta
+            self.x = self.x + (math.cos(self.theta) * self.x_traveled) - math.sin(self.theta) * self.y_traveled
+            self.y = self.y + (math.sin(self.theta) * self.x_traveled) + math.cos(self.theta) * self.y_traveled
+            rospy.loginfo("\nx_total: %.2f ",self.x)
+            rospy.loginfo("\ny_total: %.2f ",self.y)
+            self.theta = self.theta + self.theta_traveled
 
             #create quaternion since all odometry is 6 DOF
             quaternion = tf_conversions.transformations.quaternion_from_euler(0,0,self.theta)
@@ -133,7 +144,7 @@ class OdomTf:
             static_trans_stamped.transform.translation.x = self.x
             static_trans_stamped.transform.translation.y = self.y
             static_trans_stamped.transform.translation.z = 0.0
-            rospy.loginfo("\nX distance traveled: %d \nY distance %d", self.x, self.y)
+            #rospy.loginfo("\nX distance traveled: %d \nY distance %d", self.x, self.y)
             static_trans_stamped.transform.rotation.x = quaternion[0]
             static_trans_stamped.transform.rotation.y = quaternion[1]
             static_trans_stamped.transform.rotation.z = quaternion[2]
